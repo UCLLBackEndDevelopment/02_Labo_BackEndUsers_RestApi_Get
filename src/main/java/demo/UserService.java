d74582c4c510a8e0a818089a5d296a8d39e7e750
elke-steegmans <elke.steegmans@ucll.be>
2023-02-19 15:51:10 +0100

    Initial commit

---
+++ src/main/java/demo/UserService.java
@@ -0,0 +1,44 @@
+package demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private List<User> userRepository = new ArrayList<>();
+
+    public UserService() {
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository;
+    }
+
+    public List<User> getUsersWithAgeOlderThan(int age) {
+        return userRepository.stream().filter(user -> user.getAge()>age).toList();
+    }
+
+    public User getOldestUser() {
+        User oldest = null;
+        if (userRepository.size()>0) {
+            oldest = userRepository.get(0);
+            for (User user : userRepository) {
+                if (user.getAge() > oldest.getAge())
+                    oldest = user;
+            }
+        }
+        return oldest;
+    }
+
+    public User getUserWithName(String name) {
+        return userRepository.stream().filter(user -> user.getName().equals(name)).toList().get(0);
+    }
+
+    public boolean addUser(User user) {
+        return userRepository.add(user);
+    }
+
+}
